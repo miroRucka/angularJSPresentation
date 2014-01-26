b5b8d749b8f5499ba28b3d06fe5c53cbaf6e90f6
rucka <miroslav.rucka@posam.sk>
2014-01-26 20:28:02 +0100

    final modification

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 var path = require('path');
-var port = 8080;
+var port = process.env.VMC_APP_PORT || 8080;
 
 http.createServer(function (request, response) {
 
